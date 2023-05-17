a017e5b0dd85d0e48ef9165b166e5ca36531d73a
Jake Overall <joverall22@gmail.com>
2023-05-17 15:58:03 -0600

    small fix

--- app/router.js
+++ app/router.js
@@ -1,9 +1,14 @@
-import { UserSpellsController } from "../../app/controllers/UserSpellsController.js";
+import { AboutController } from "./controllers/AboutController.js";
 import { SpellsController } from "./controllers/SpellsController.js";
+import { UserSpellsController } from "./controllers/UserSpellsController.js";
 
 export const router = [
   {
     path: '',
     controller: [SpellsController, UserSpellsController]
+  },
+  {
+    path: '#/about',
+    controller: AboutController
   }
 ]
